ecbb7722c8097ecf3a7abe226e463faadd0f4dc2
Berna019 <2011364@cesba-queretaro.edu.mx>
2023-04-06 20:32:32 -0600

    Primer cambio

--- index.php
+++ index.php
@@ -8,11 +8,41 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <title></title>
+        <link rel="shortcut icon" href="../img/icoSIITEB.ico">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     </head>
     <body>
-        <?php
-        echo 'Mi primera prueba con Heroku';
-        // put your code here
-        ?>
+        <div class="container">
+            <br>
+            <form class="row" style="margin-left: 400px ;" action="#" method="POST">
+                <fieldset>
+                    <div class="col-6">
+                        <label for="">Nombre Completo</label>
+                        <input type="text" class="form-control" name="Nombre">
+                    </div>
+                    <br>
+                    <div class="col-6">
+                        <label class="">Telefono</label>
+                        <input type="text" class="form-control" name="apellidoPaterno">
+                    </div>
+                    <br>
+                    <div class="col-6">
+                        <label class="">Asunto</label>
+                        <input type="text" class="form-control" name="apellidoMaterno">
+                    </div>
+                    <br>
+                    <div class="col-6">
+                        <label class="">Mensaje</label>
+                        <input type="text" class="form-control" name="Telefono">
+                    </div>
+                    <br>
+                    <div class="vstack gap-2 col-6">
+                        <button type="submit" class="btn btn-dark col-6 d-grid mx-auto">Enviar</button>
+                        <a href="#" class="btn btn-outline-dark col-6 d-grid mx-auto">Cancelar</a>
+                    </div>
+                </fieldset>
+            </form>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     </body>
 </html>
