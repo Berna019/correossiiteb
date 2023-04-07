2c1d3974f777b4491d91de4d224ac24a61fbc513
Berna019 <2011364@cesba-queretaro.edu.mx>
2023-04-06 20:45:43 -0600

    Cambio 2

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Contacto</title>
         <link rel="shortcut icon" href="../img/icoSIITEB.ico">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     </head>
@@ -22,7 +22,7 @@ and open the template in the editor.
                     </div>
                     <br>
                     <div class="col-6">
-                        <label class="">Telefono</label>
+                        <label class="">Telefóno</label>
                         <input type="text" class="form-control" name="apellidoPaterno">
                     </div>
                     <br>
