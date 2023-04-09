60d904426c8c34509705a42ef1716c394b5c3798
Berna019 <2011364@cesba-queretaro.edu.mx>
2023-04-08 21:15:05 -0600

    Cambio 13

--- Vistas/consultas.php
+++ Vistas/consultas.php
@@ -12,6 +12,7 @@ $sql = $conexion->query("select * from contacto");
 <html>
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no,initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
         <title>Contacto</title>
         <link rel="shortcut icon" href="Imagenes/icoSIITEB.ico">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
@@ -20,7 +21,7 @@ $sql = $conexion->query("select * from contacto");
     <body>
         <nav class="container navbar navbar-expand-md ">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">SIITEB </a>
+                <a class="navbar-brand" href="#">SIITEB</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
