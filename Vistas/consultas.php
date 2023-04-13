d882f6b0162f60624c6bb4d6287391cb21c2eac9
Berna019 <2011364@cesba-queretaro.edu.mx>
2023-04-12 21:03:17 -0600

    cambio 4

--- Vistas/consultas.php
+++ Vistas/consultas.php
@@ -11,67 +11,85 @@ $sql = $conexion->query("select * from contacto");
 ?>
 <html>
     <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, user-scalable=no,initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-        <title>Contacto</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>contacto</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
         <link rel="shortcut icon" href="Imagenes/icoSIITEB.ico">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-        <link rel="stylesheet" href="styles/styleEnviar.css">
+        <link rel="stylesheet" href="../styles/consultas.css">
     </head>
     <body>
-        <nav class="container navbar navbar-expand-md ">
+        <nav class="container navbar navbar-dark bg-dark navbar-expand-lg navbar-light bg-light fixed-top">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">SIITEB</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#menu" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <form class="d-flex">
+                    <input class="form-control me-3" type="search" placeholder="Buscar" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Buscar</button>
+                </form>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="menu">
-                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="../Vistas/contacto.php">Contacto</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="../index.php">Salir</a>
                         </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Dropdown
+                            </a>
+                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><a class="dropdown-item" href="#">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li><hr class="dropdown-divider"></li>
+                                <li><a class="dropdown-item" href="#">Something else here</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                        </li>
                     </ul>
                 </div>
             </div>
         </nav>
-        <table class="container table table-striped-columns">
-            <thead>
-                <tr>
-                    <th scope="col">Folio</th>
-                    <th scope="col">Nombre completo</th>
-                    <th scope="col">Telefono</th>
-                    <th scope="col">Correo</th>
-                </tr>
-            </thead>
-            <?php
-            $no = 1;
-            while ($row = $sql->fetch_assoc()) {
-                echo
-                //inicia html
-                "
+        <div class="container table-responsive mt-6">
+            <table class="table table-sm align-middle">
+                <thead>
+                    <tr>
+                        <th scope="col">Folio</th>
+                        <th scope="col">Nombre completo</th>
+                        <th scope="col">Telefono</th>
+                        <th scope="col">Correo</th>
+                        <th scope="col">Asunto</th>
+                    </tr>
+                </thead>
+                <?php
+                $no = 1;
+                while ($row = $sql->fetch_assoc()) {
+                    echo
+                    //inicia html
+                    "
         <tr>
         <td>{$row['idcontacto']}</td>
         <td>{$row['nombre_completo']}</td>
         <td>{$row['telefono']}</td>
         <td>{$row['correo']}</td>
+        <td>{$row['asunto']}</td>
         </tr>
         ";
-                +
-                        $no ++;
-                //termina html
-            }
-            ?>
-        </table>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+                    +
+                            $no ++;
+                    //termina html
+                }
+                ?>
+            </table>
+        </div>
+        <div class="container-fluid text-center mt-5">
+            <p class="small">$copyright: Todos los drechos reservados</p>
+        </div>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
     </body>
 </html>
-
-<title>Consultas</title>
-<link rel="shortcut icon" href="../Imagenes/icoSIITEB.ico">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
-
